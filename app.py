3c6fb4a982b6208faf99f4bc80325d1a2ef00380
MrNovaie <158982495+MrNovaie@users.noreply.github.com>
2026-03-26 11:31:17 +0800

    Change app host from 'localhost' to '0.0.0.0'

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def api_debug():
 if __name__ == '__main__':
     with app.app_context():  # Ensure the application context is available for database operations
         db.create_all()
-    app.run(host='localhost', debug=True)
+    app.run(host='0.0.0.0', debug=True)
