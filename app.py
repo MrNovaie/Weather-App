17f678c38226d751ac7fe1da76a14f27503c22b4
MrNovaie <158982495+MrNovaie@users.noreply.github.com>
2026-03-26 13:10:33 +0800

    Fix database URL assignment for SQLAlchemy

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ db_url = os.getenv('DATABASE_URL')
 if db_url and db_url.startswith("postgres://"):
     db_url = db_url.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(db_url)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable track modifications to save resources
 db = SQLAlchemy(app)
 
