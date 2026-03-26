3c22b9e5b561ac84ab82a3660f472129aaf59d65
MrNovaie <158982495+MrNovaie@users.noreply.github.com>
2026-03-26 12:59:03 +0800

    Disable debug mode in app.run()
    Removed debug mode from app.run()

--- app.py
+++ app.py
@@ -140,4 +140,4 @@ def api_debug():
 if __name__ == '__main__':
     with app.app_context():  # Ensure the application context is available for database operations
         db.create_all()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
